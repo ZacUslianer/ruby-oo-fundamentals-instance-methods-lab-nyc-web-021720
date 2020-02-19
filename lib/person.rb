89eeed684f314701e5b414dae72c9395c40a9a37
ZacUslianer <zacuslianer@gmail.com>
2020-02-19 15:50:49 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+    def talk
+        puts "Hello World!"
+    end
+end
+
+josh = Person.new
+josh.talk
+
+class Person
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+billie = Person.new
+billie.walk
